7337c960baa5819a9d7a17e9ee52d27a303ce5e1
JackWhite-rwx <56493382+JackWhite-rwx@users.noreply.github.com>
2023-01-14 12:59:54 +0800

    Update gradio_cpu.py

--- gradio_cpu.py
+++ gradio_cpu.py
@@ -38,12 +38,12 @@ def detr_resnet50(pretrained=False, num_classes=91, return_postprocessor=False):
     """
     model = _make_detr("resnet50", dilation=False, num_classes=num_classes)
     if pretrained:
-        # checkpoint = torch.hub.load_state_dict_from_url(
-        #     url="https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth", map_location="cpu", check_hash=True
-        # )
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth", map_location="cpu", check_hash=True
+        )
         print("load local pretrained object detection para")
-        checkpoint = torch.load("./checkpoints/detr-r50.pth")
-        model.load_state_dict(checkpoint["model"])
+        #checkpoint = torch.load("./checkpoints/detr-r50.pth")
+        #model.load_state_dict(checkpoint["model"])
     if return_postprocessor:
         return model, PostProcess()
     return model
